b662e2741e33c3f2b5ac8653c770e73816d04851
Mazharul <mazharulslm9@gmail.com>
2022-02-11 01:41:58 +0600

    728x90

---
+++ Code/728x90/728x90_BC_feed_Feb2021/manifest.js
@@ -0,0 +1,35 @@
+FT.manifest({
+    "filename": "index.html",
+    "width": 728,
+    "height": 90,
+    "clickTagCount": 1,
+    "hideBrowsers": ["ie11"],
+    "instantAds": [
+
+        { "name": "coverBackground", "type": "image", "default": "img/728x90_natural_health.png" },
+        { "name": "coverHold", "type": "text", "default": "2" },
+        { "name": "numOfLoops", "type": "text", "default": "1" },
+
+        { "name": "carouselArrow", "type": "image", "default": "img/728x90_nav_arrow.png" },
+        { "name": "carouselCTA", "type": "text", "default": "shop now" },
+        { "name": "carouselCTAColour", "type": "text", "default": "#ec2927" },
+
+        { "name": "carouselHeadline", "type": "image", "default": "img/728x90_flag.png" },
+
+        { "name": "segmentId", "type": "text", "default": "false" },
+        { "name": "feedEndpoint", "type": "text", "default": "https://fm.flashtalking.com/feed/1572/trending/master?ftCreds=1&numItems=7" },
+        { "name": "defaultFeedEndpoint", "type": "text", "default": "https://fm.flashtalking.com/feed/1554/trending/master" },
+
+        { "name": "backgroundImage", "type": "image", "default": "img/blank.png" },
+        { "name": "colourBlock", "type": "text", "default": "#25408f" },
+
+        { "name": "genericBackup", "type": "image", "default": "img/728x90_natural_health.png" },
+        { "name": "genericClickthrough", "type": "text", "default": "https://www.thewarehousegroup.co.nz/" },
+
+        { "name": "terms_button", "type": "text", "default": "[%image%] FREE SHIPPING" },
+        { "name": "terms_image", "type": "image", "default": "img/arrow.png" },
+        { "name": "promo_image", "type": "image", "default": "img/728x90_endframe_fs.png" },
+        { "name": "term_reveal_type", "type": "text", "default": "click" }
+
+    ]
+});
